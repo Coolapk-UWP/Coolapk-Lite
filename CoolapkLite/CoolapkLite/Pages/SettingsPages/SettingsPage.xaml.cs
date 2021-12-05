9f990b7c4e2e1828604ef4b79ae87b31f871b9ff
wherewhere <qq2518766683@outlook.com>
2021-12-05 20:39:20 +0800

    现在Pivot是WinUI的样子了

--- CoolapkLite/CoolapkLite/Pages/SettingsPages/SettingsPage.xaml.cs
+++ CoolapkLite/CoolapkLite/Pages/SettingsPages/SettingsPage.xaml.cs
@@ -7,6 +7,7 @@ using Windows.Storage;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
 using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
--- CoolapkLite/CoolapkLite/Pages/SettingsPages/TestPage.xaml.cs
+++ CoolapkLite/CoolapkLite/Pages/SettingsPages/TestPage.xaml.cs
@@ -8,6 +8,8 @@ using Windows.System;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
