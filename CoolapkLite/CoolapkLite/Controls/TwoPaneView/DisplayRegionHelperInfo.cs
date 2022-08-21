986aa9925029562cd2d49db029ba4620649afed1
wherewhere <qq2518766683@outlook.com>
2022-08-21 17:10:42 +0800

    Update DisplayRegionHelperInfo.cs

--- CoolapkLite/CoolapkLite/Controls/TwoPaneView/DisplayRegionHelperInfo.cs
+++ CoolapkLite/CoolapkLite/Controls/TwoPaneView/DisplayRegionHelperInfo.cs
@@ -4,7 +4,14 @@ namespace CoolapkLite.Controls
 {
     public struct DisplayRegionHelperInfo
     {
+        private const int c_maxRegions = 2;
+
         public TwoPaneViewMode Mode { get; set; }
         public Rect[] Regions { get; set; }
+
+        public DisplayRegionHelperInfo()
+        {
+            Regions = new Rect[c_maxRegions];
+        }
     }
 }
