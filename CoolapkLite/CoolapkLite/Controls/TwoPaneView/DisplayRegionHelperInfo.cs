fb46f393d9685692ac698e182483f01f2583d32f
wherewhere <qq2518766683@outlook.com>
2022-08-23 00:26:39 +0800

    修复双屏显示

--- CoolapkLite/CoolapkLite/Controls/TwoPaneView/DisplayRegionHelperInfo.cs
+++ CoolapkLite/CoolapkLite/Controls/TwoPaneView/DisplayRegionHelperInfo.cs
@@ -2,16 +2,11 @@
 
 namespace CoolapkLite.Controls
 {
-    public struct DisplayRegionHelperInfo
+    public class DisplayRegionHelperInfo
     {
         private const int c_maxRegions = 2;
 
         public TwoPaneViewMode Mode { get; set; }
-        public Rect[] Regions { get; set; }
-
-        public DisplayRegionHelperInfo()
-        {
-            Regions = new Rect[c_maxRegions];
-        }
+        public Rect[] Regions { get; set; } = new Rect[c_maxRegions];
     }
 }
