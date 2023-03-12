83c30eb3ae09a68462c04783e2a995ddd9c7820b
wherewhere <qq2518766683@outlook.com>
2023-03-13 00:02:17 +0800

    分离 Style

--- CoolapkLite/CoolapkLite/Helpers/Controls/FlyoutBaseHelper.cs
+++ CoolapkLite/CoolapkLite/Helpers/Controls/FlyoutBaseHelper.cs
@@ -1,10 +1,11 @@
 ﻿using Windows.Foundation.Metadata;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 
 namespace CoolapkLite.Helpers
 {
-    public static class MenuFlyoutHelper
+    public static class FlyoutBaseHelper
     {
         #region Icon
 
@@ -35,7 +36,7 @@ namespace CoolapkLite.Helpers
             DependencyProperty.RegisterAttached(
                 "Icon",
                 typeof(IconElement),
-                typeof(MenuFlyoutHelper),
+                typeof(FlyoutBaseHelper),
                 new PropertyMetadata(null, OnIconChanged));
 
         private static void OnIconChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
@@ -59,6 +60,51 @@ namespace CoolapkLite.Helpers
                 if (e.NewValue is IconElement IconElement && ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.ToggleMenuFlyoutItem", "Icon"))
                 {
                     toggle.Icon = IconElement;
+                }
+            }
+        }
+
+        #endregion
+
+        #region ShouldConstrainToRootBounds
+
+        /// <summary>
+        /// Gets a value that indicates whether the flyout should be shown within the bounds of the XAML root.
+        /// </summary>
+        /// <param name="control">The element from which to read the property value.</param>
+        /// <returns>A value that indicates whether the flyout should be shown within the bounds of the XAML root.</returns>
+        public static bool GetShouldConstrainToRootBounds(FlyoutBase control)
+        {
+            return (bool)control.GetValue(ShouldConstrainToRootBoundsProperty);
+        }
+
+        /// <summary>
+        /// Sets a value that indicates whether the flyout should be shown within the bounds of the XAML root.
+        /// </summary>
+        /// <param name="control">The element on which to set the attached property.</param>
+        /// <param name="value">The property value to set.</param>
+        public static void SetShouldConstrainToRootBounds(FlyoutBase control, bool value)
+        {
+            control.SetValue(ShouldConstrainToRootBoundsProperty, value);
+        }
+
+        /// <summary>
+        /// Identifies the ShouldConstrainToRootBounds dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ShouldConstrainToRootBoundsProperty =
+            DependencyProperty.RegisterAttached(
+                "ShouldConstrainToRootBounds",
+                typeof(bool),
+                typeof(FlyoutBaseHelper),
+                new PropertyMetadata(true, OnShouldConstrainToRootBoundsChanged));
+
+        private static void OnShouldConstrainToRootBoundsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is FlyoutBase flyout)
+            {
+                if (e.NewValue is bool ShouldConstrainToRootBounds && ApiInformation.IsPropertyPresent("Windows.UI.Xaml.Controls.Primitives.FlyoutBase", "ShouldConstrainToRootBounds"))
+                {
+                    flyout.ShouldConstrainToRootBounds = ShouldConstrainToRootBounds;
                 }
             }
         }
