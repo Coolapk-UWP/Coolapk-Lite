62275d9abdc8db061655dc704567be76f1ed5620
wherewhere <qq2518766683@outlook.com>
2023-04-21 21:44:47 +0800

    优化查看图片

--- CoolapkLite/CoolapkLite/Pages/ShowImagePage.xaml.cs
+++ CoolapkLite/CoolapkLite/Pages/ShowImagePage.xaml.cs
@@ -4,10 +4,12 @@ using CoolapkLite.ViewModels;
 using Microsoft.Toolkit.Uwp.Helpers;
 using Windows.ApplicationModel.Core;
 using Windows.ApplicationModel.DataTransfer;
+using Windows.Foundation;
 using Windows.Foundation.Metadata;
 using Windows.Phone.UI.Input;
 using Windows.System.Profile;
 using Windows.UI.Core;
+using Windows.UI.Input;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
@@ -22,6 +24,7 @@ namespace CoolapkLite.Pages
     /// </summary>
     public sealed partial class ShowImagePage : Page
     {
+        private Point _clickPoint = new Point(0, 0);
         private ShowImageViewModel Provider;
 
         public ShowImagePage()
@@ -146,6 +149,30 @@ namespace CoolapkLite.Pages
             }
         }
 
+        private void ScrollViewer_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            CommandBar.Visibility = CommandBar.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void ScrollViewer_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+        {
+            ScrollViewer scrollViewer = sender as ScrollViewer;
+            Point doubleTapPoint = e.GetPosition(scrollViewer);
+
+            _ = scrollViewer.ZoomFactor != 1
+                ? scrollViewer.ChangeView(0, 0, 1)
+                : scrollViewer.ChangeView(doubleTapPoint.X, doubleTapPoint.Y, 2);
+
+            CommandBar.Visibility = CommandBar.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        private void ScrollViewer_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+            ScrollViewer scrollViewer = sender as ScrollViewer;
+            FrameworkElement element = scrollViewer.Content as FrameworkElement;
+            element.CanDrag = scrollViewer.ZoomFactor <= 1;
+        }
+
         private async void Image_DragStarting(UIElement sender, DragStartingEventArgs args)
         {
             args.DragUI.SetContentFromDataPackage();
@@ -153,10 +180,39 @@ namespace CoolapkLite.Pages
             await Provider.GetImageDataPackage(args.Data, "拖拽图片");
         }
 
+        private void Image_PointerPressed(object sender, PointerRoutedEventArgs e)
+        {
+            FrameworkElement element = sender as FrameworkElement;
+            PointerPoint pointerPoint = e.GetCurrentPoint(element);
+            if (pointerPoint.Properties.IsLeftButtonPressed)
+            {
+                _clickPoint = e.GetCurrentPoint(element).Position;
+            }
+        }
+
+        private void Image_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            FrameworkElement element = sender as FrameworkElement;
+            ScrollViewer scrollViewer = element.Parent as ScrollViewer;
+            PointerPoint pointerPoint = e.GetCurrentPoint(element);
+            if (pointerPoint.Properties.IsLeftButtonPressed)
+            {
+                double x, y;
+                Point point = e.GetCurrentPoint(element).Position;
+                x = _clickPoint.X - point.X;
+                y = _clickPoint.Y - point.Y;
+                _ = scrollViewer.ChangeView(scrollViewer.HorizontalOffset + x, scrollViewer.VerticalOffset + y, null);
+            }
+        }
+
+        private void Image_ManipulationDelta(object sender, ManipulationDeltaRoutedEventArgs e)
+        {
+            ScrollViewer view = (sender as FrameworkElement).Parent as ScrollViewer;
+            _ = view.ChangeView(view.HorizontalOffset - (e.Delta.Translation.X * view.ZoomFactor), view.VerticalOffset - (e.Delta.Translation.Y * view.ZoomFactor), null);
+        }
+
         private void TitleBar_IsVisibleChanged(CoreApplicationViewTitleBar sender, object args) => UpdateContentLayout(sender.IsVisible);
 
         private void TitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args) => UpdateTitleBarLayout(sender);
-
-        private void FlipView_Tapped(object sender, TappedRoutedEventArgs e) => CommandBar.Visibility = CommandBar.Visibility == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
     }
 }
