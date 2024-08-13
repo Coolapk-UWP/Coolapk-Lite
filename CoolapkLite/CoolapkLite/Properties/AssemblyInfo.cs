1433f3cc792af997c178f8d90fdfb3b5a3d9444f
wherewhere <qq2518766683@outlook.com>
2024-08-13 19:32:29 +0800

    更新 v0.0.3

--- CoolapkLite/CoolapkLite/Properties/AssemblyInfo.cs
+++ CoolapkLite/CoolapkLite/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Coolapk UWP Project")]
 [assembly: AssemblyProduct("CoolapkLite")]
-[assembly: AssemblyCopyright("Copyright © 2017 - 2023 Coolapk UWP Project. All Rights Reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2017 - 2024 Coolapk UWP Project. All Rights Reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -23,6 +23,6 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.2.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
 [assembly: ComVisible(false)]
